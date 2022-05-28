f7bfc7c89e8459a24b7467431c9ee5bbb0f60e1f
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 18:40:34 +0300

    [script.js] addid refs

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
