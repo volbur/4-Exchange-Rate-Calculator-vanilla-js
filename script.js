2e44ec2a2e3a752cd1cb0462c5d1e3cf7b3c55f5
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-28 18:56:24 +0300

    [script.js] fix syntax errors

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 function calculate() {
-  const currency_one = currencyEI_one.value;
+  const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
   fetch("https://open.exchangerate-api.com/v6/latest")
     .then((res) => res.json())
     .then((data) => {
-      const rate = data.rates[currency_two] / data.rates[currencyEl_one];
+      const rate = data.rates[currency_two] / data.rates[currency_one];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
     });
 }
 
